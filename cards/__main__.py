0a6669bc7c46f8de67ea6f09e680064814c1bce8
James Power <jep@lepoer.us>
2021-11-16 21:46:44 -0500

    Dumping 3x3 grid to a pdf

--- cards/__main__.py
+++ cards/__main__.py
@@ -1,5 +1,51 @@
-#!/usr/bin/env python3
+import os, sys, fitz
+print(fitz.__doc__)
 
-import fitz
+imgdir = sys.argv[1]
 
-print('hello')
+doc = fitz.open()
+
+width, height = fitz.paper_size("letter")
+imglist = os.listdir(imgdir)
+imglist = imglist + imglist + imglist
+imglist.sort()
+imgcount = len(imglist)
+
+page = doc.new_page(width=width, height=height)
+# pdf_path = os.path.join(imgdir, 'template.pdf')
+# doc = fitz.open()
+
+# 2.5/3.5 = 0.7142857143
+# 1572 wide, 2038 tall
+horizontal_padding = width * (1/17)
+vertical_padding = height * (1/44)
+card_width = (width - (horizontal_padding * 2)) / 3
+card_height = (height - (vertical_padding * 2)) / 3
+count = 0
+row = 0
+column = 0
+for i, f in enumerate(imglist):
+    path = os.path.join(imgdir, f)
+    if not os.path.isfile(path) or 'DS_Store' in path or 'pdf' in path:
+        continue
+    print(f'index: {count}, row: {row}, column: {column}')
+    x0 = column * card_width + horizontal_padding
+    x1 = x0 + card_width
+    y0 = row * card_height  + vertical_padding
+    y1 = y0 + card_height
+
+    img = fitz.open(path)
+    rect = (x0, y0, x1, y1)
+    print(f'rect: {rect}')
+    pdfbytes = img.convert_to_pdf()  # make a PDF stream
+    img.close()  # no longer needed
+    imgPDF = fitz.open("pdf", pdfbytes)  # open stream as PDF
+    # for a new page call
+    #page = doc.new_page(width = rect.width, height = rect.height)
+    page.show_pdf_page(rect, imgPDF, 0)  # image fills the page
+    count = count + 1
+    if count == 3 or count == 6:
+        row = row + 1
+    column = count % 3
+
+doc.save("all-my-pics-embedded.pdf")
