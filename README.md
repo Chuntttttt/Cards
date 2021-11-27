2b60e1cc11e182e8f57c339037b87b8c0e736e67
James Power <jep@lepoer.us>
2021-11-27 12:54:37 -0500

    Add pipelines

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Cards
+
+[![macOS](https://github.com/Chuntttttt/Cards/actions/workflows/macos/badge.svg)](https://github.com/Chuntttttt/Cards/actions/workflows/macos.yml) [![Ubuntu](https://github.com/Chuntttttt/Cards/actions/workflows/ubuntu/badge.svg)](https://github.com/Chuntttttt/Cards/actions/workflows/ubuntu.yml) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Requirements
+
 Requires MuPDF, available on macOS via homebrew:
 
 `$ brew install mupdf swig freetype`
@@ -5,4 +11,3 @@ Requires MuPDF, available on macOS via homebrew:
 Adapted the MuPDF sample: https://github.com/pymupdf/PyMuPDF-Utilities/blob/master/examples/all-my-pics-embedded.py
 
 Uses [poetry](https://python-poetry.org) for dependency management.
-
---
+++ cards/cards.py
@@ -0,0 +1,92 @@
+import logging
+import os
+
+# import sys
+
+import fitz
+from fitz import Point
+from fitz import Rect
+
+
+def draw_guides(
+    shape, x0: int, y0: float, x1: float, y1: float, width: float, height: float
+):
+    # This all could've been done more elegantly I am certain.
+    logging.debug(f'points ({x0}, {y0})\t({x1}, {y1})')
+    size = 20
+    shape.draw_line(Point(x0, y0 - size), Point(x0, y0 + size))
+    shape.draw_line(Point(x0 - size, y0), Point(x0 + size, y0))
+    shape.draw_line(Point(x1, y1 - size), Point(x1, y1 + size))
+    shape.draw_line(Point(x1 - size, y1), Point(x1 + size, y1))
+    shape.draw_line(Point(x0, y1 - size), Point(x0, y1 + size))
+    shape.draw_line(Point(x0 - size, y1), Point(x0 + size, y1))
+    shape.draw_line(Point(x1, y0 - size), Point(x1, y0 + size))
+    shape.draw_line(Point(x1 - size, y0), Point(x1 + size, y0))
+    # lol at these hard coded values
+    if y0 < 20:
+        shape.draw_line(Point(x0, 0), Point(x0, y0))
+        shape.draw_line(Point(x1, 0), Point(x1, y0))
+    if y0 > 500:
+        shape.draw_line(Point(x0, height), Point(x0, y1))
+        shape.draw_line(Point(x1, height), Point(x1, y1))
+    if x0 < 40:
+        shape.draw_line(Point(0, y0), Point(x0, y0))
+        shape.draw_line(Point(0, y1), Point(x0, y1))
+    if x0 > 390:
+        shape.draw_line(Point(x1, y0), Point(width, y0))
+        shape.draw_line(Point(x1, y1), Point(width, y1))
+
+    shape.finish()
+    shape.commit()
+
+
+def main():
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    # imgdir = sys.argv[1]
+    imgdir = 'static/cards'
+    doc = fitz.open()
+
+    width, height = fitz.paper_size('letter')
+    imglist = os.listdir(imgdir + '/front')
+    columns = 3
+    rows = 3
+    page = doc.new_page(width=width, height=height)
+    horizontal_padding = width * (1 / 17)
+    vertical_padding = height * (1 / 44)
+    card_width = (width - (horizontal_padding * 2)) / columns
+    card_height = (height - (vertical_padding * 2)) / rows
+    count = 0
+    row = 0
+    column = 0
+    for i, f in enumerate(imglist):
+        path = os.path.join(imgdir, f)
+        if not os.path.isfile(path) or 'DS_Store' in path or 'pdf' in path:
+            continue
+        x0 = column * card_width + horizontal_padding
+        x1 = x0 + card_width
+        y0 = row * card_height + vertical_padding
+        y1 = y0 + card_height
+
+        img = fitz.open(path)
+        rect = Rect(x0, y0, x1, y1)
+        pdfbytes = img.convert_to_pdf()
+        img.close()
+        imgPDF = fitz.open('pdf', pdfbytes)
+        page.show_pdf_page(rect, imgPDF, 0)
+        count = count + 1
+        if count % columns == 0:
+            row = row + 1
+        column = count % columns
+
+        shape = page.new_shape()
+        draw_guides(shape, x0, y0, x1, y1, width, height)
+
+    # for a new page call
+    # page = doc.new_page(width = width, height = height)
+    doc.save('cards.pdf')
+
+
+# i'm not sure how to tell vscode to run __main__.py lmao
+if __name__ == '__main__':
+    main()
