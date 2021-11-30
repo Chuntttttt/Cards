fc7123824db95401794337f06b35d1dc5667128c
James Power <jep@lepoer.us>
2021-11-30 14:03:11 -0500

    align back cards appropriately

--- cards/cards.py
+++ cards/cards.py
@@ -57,11 +57,15 @@ class CardWriter:
         if x0 > 390:
             shape.draw_line(Point(x1, y0), Point(self.width, y0))
             shape.draw_line(Point(x1, y1), Point(self.width, y1))
-
         shape.finish()
         shape.commit()
 
-    def __images_from_path(self, images_path) -> List[str]:
+    def __group_images(self, images: List[str]):
+        groupedRows = grouper(images, self.side_size)
+        groupedPages = grouper(groupedRows, self.side_size)
+        return groupedPages
+
+    def __images_from_path(self, images_path):
         """
         looks for png, jpg, jpeg, extensions and ignores other files in the given path
         """
@@ -75,11 +79,6 @@ class CardWriter:
             ):
                 images.append(path)
         return self.__group_images(sorted(images))
-
-    def __group_images(self, images: List[str]) -> List[List[str]]:
-        groupedRows = grouper(images, self.side_size)
-        groupedPages = grouper(groupedRows, self.side_size)
-        return groupedPages
 
     def __add_images(self, images: List[List[str]]):
         pdf_page = self.doc.new_page(width=self.width, height=self.height)
@@ -104,9 +103,26 @@ class CardWriter:
                         # sheet. row_index & image_index should be enough here.
                         self.__draw_guides(shape, x0, y0, x1, y1)
 
+    def __align_back_cards(self, back_pages):
+        cards = []
+        for page in back_pages:
+            if page is None:
+                cards.append(page)
+                continue
+            page_cards = []
+            cards.append(page_cards)
+            for row in page:
+                if row is None:
+                    page_cards.append(row)
+                    continue
+                page_cards.append(reversed(list(row)))
+        return cards
+
+
     def create_pdf(self):
         self.doc = fitz.open()
         front_cards = self.__images_from_path(self.cards_path + '/front')
+        back_cards = self.__align_back_cards(self.__images_from_path(self.cards_path + '/back'))
         for image_page in front_cards:
             self.__add_images(image_page)
         self.doc.save(self.output)
